ae87283aee5b1e72ea77f0e0c55c6908b513f622
Anne Bischof <7795449+ambischof@users.noreply.github.com>
2024-01-20 18:27:14 -0600

    Fixing css not working in deployed version

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   // make a static output at this stage
   output: 'export',
   // github pages needs to point to either root or `/docs`, so thus it is
-  distDir: 'docs'
+  distDir: 'docs',
+  // fixes css not working
+  assetPrefix: './'
 }
 
 module.exports = nextConfig
