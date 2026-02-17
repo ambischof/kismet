7b33bb32f7d4a74e56db6073315823377a3d8c94
Anne Bischof <7795449+ambischof@users.noreply.github.com>
2026-02-17 16:22:06 -0600

    moving deployment folder back to out

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   // make a static output at this stage
   output: 'export',
-  // github pages needs to point to either root or `/docs`, so thus it is
-  distDir: 'docs',
   // fixes css not working
   assetPrefix: './'
 }
